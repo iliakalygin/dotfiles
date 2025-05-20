aebf6754294507e048427c7f22589ff08bea8184
iliakalygin <58369822+iliakalygin@users.noreply.github.com>
2025-05-20 23:05:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@ sudo pacman -S kitty zsh wofi waybar nvim
 
 mv ~/.config/sway ~/.config/sway.bak
 
+ 
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/zsh
+mkdir -p ~/.config/wofi
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/nvim
+
 ln -s ~/git/dotfiles/kitty ~/.config/kitty
 ln -s ~/git/dotfiles/sway ~/.config/sway
 ln -s ~/git/dotfiles/zsh ~/.config/zsh
