069f8c1020cb52e843631cc1931365a29d48eb59
iliakalygin <58369822+iliakalygin@users.noreply.github.com>
2025-05-21 14:42:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S kitty zsh wofi waybar nvim
+sudo pacman -S kitty zsh wofi waybar nvim starship
 
 ln -s ~/git/dotfiles/kitty ~/.config/kitty
 ln -s ~/git/dotfiles/sway ~/.config/sway
@@ -6,3 +6,7 @@ ln -s ~/git/dotfiles/zsh ~/.config/zsh
 ln -s ~/git/dotfiles/wofi ~/.config/wofi
 ln -s ~/git/dotfiles/waybar ~/.config/waybar
 ln -s ~/git/dotfiles/nvim ~/.config/nvim
+ln -s ~/git/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/git/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -s ~/git/dotfiles/git ~/.config/git
+
