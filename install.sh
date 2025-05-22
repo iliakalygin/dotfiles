09ae25b8c51a7e22a8aeac0c90aa37e7e2ef5f5b
iliakalygin <58369822+iliakalygin@users.noreply.github.com>
2025-05-23 00:27:34 +0200

    update intsall.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S kitty zsh wofi waybar nvim starship
+sudo pacman -S kitty zsh wofi waybar nvim starship fastfetch tmux
 
 ln -s ~/git/dotfiles/kitty ~/.config/kitty
 ln -s ~/git/dotfiles/sway ~/.config/sway
@@ -8,5 +8,8 @@ ln -s ~/git/dotfiles/waybar ~/.config/waybar
 ln -s ~/git/dotfiles/nvim ~/.config/nvim
 ln -s ~/git/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/git/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -s ~/git/dotfiles/git ~/.config/git
+ln -s ~/git/dotfiles/fastfetch ~/.config/fastfetch
+ln -s ~/git/dotfiles/tmux ~/.config/tmux
 ln -s ~/git/dotfiles/git ~/.config/git
 
