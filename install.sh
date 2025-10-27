766ea482f29a3884111e357e9c5dbf9747392cfb
Ilia Kalygin <ilia.kalygin@protonmail.com>
2025-10-27 11:25:37 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo xbps-install -Syv kitty zsh wofi Waybar neovim starship fastfetch tmux lsd lazygit gnome-themes-extra pcmanfm fzf xcursor-themes xrdb
+sudo xbps-install -Syv kitty zsh wofi Waybar neovim starship fastfetch tmux lsd lazygit gnome-themes-extra fzf xcursor-themes xrdb
 
 ln -s /home/void/git/dotfiles/kitty /home/void/.config
 ln -s /home/void/git/dotfiles/sway /home/void/.config
@@ -14,3 +14,20 @@ ln -s /home/void/git/dotfiles/tmux /home/void/.config
 ln -s /home/void/git/dotfiles/gtk-3.0 /home/void/.config
 ln -s /home/void/git/dotfiles/gtk-4.0 /home/void/.config
 ln -s /home/void/git/dotfiles/xresources/.Xresources /home/void/.Xresources
+
+
+
+ln -s /home/harada/git/dotfiles/kitty /home/harada/.config
+ln -s /home/harada/git/dotfiles/sway /home/harada/.config
+ln -s /home/harada/git/dotfiles/zsh /home/harada/.config
+ln -s /home/harada/git/dotfiles/wofi /home/harada/.config
+ln -s /home/harada/git/dotfiles/waybar /home/harada/.config
+ln -s /home/harada/git/dotfiles/nvim /home/harada/.config
+ln -s /home/harada/git/dotfiles/zsh/.zshrc /home/harada/.zshrc
+ln -s /home/harada/git/dotfiles/starship/starship.toml /home/harada/.config/starship.toml
+ln -s /home/harada/git/dotfiles/git /home/harada/.config
+ln -s /home/harada/git/dotfiles/fastfetch /home/harada/.config
+ln -s /home/harada/git/dotfiles/tmux /home/harada/.config
+ln -s /home/harada/git/dotfiles/gtk-3.0 /home/harada/.config
+ln -s /home/harada/git/dotfiles/gtk-4.0 /home/harada/.config
+ln -s /home/harada/git/dotfiles/xresources/.Xresources /home/harada/.Xresources
